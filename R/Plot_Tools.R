08355989e900e313b84c08a689147d20ccff7519
Sierra Johnson <SJohnson2@BrwnCald.com>
2023-02-20 13:19:32 -0700

    Add theme, clean up box and whisker plot

--- R/Plot_Tools.R
+++ R/Plot_Tools.R
@@ -141,17 +141,21 @@ scale_color_bc <- function(palette = "primary", discrete = TRUE, reverse = FALSE
 
 #' Box and Whisker Plot function that uses 25th and 75th percentile for the box and 5th and 95th percentiles for whiskers.
 #' The middle point can be at the mean or 90th percentile.
-#' Outlier points can be included or excluded.
+#' Outlier points can be included or excluded.  To set fill to a single color, include the argument fill =.
 #'
 #'
 #' @param outlier Can be set to FALSE to remove points <5th percentile and >95th percentile.
 #' @param count Can be set to FALSE to remove count from below the whisker.
 #' @param middlepoint Can be equal to "mean" (default) or "90th" to select location, can be set to FALSE to remove.
 #' @param whiskerbar Can be set to FALSE to remove horizontal bars on the ends of the whiskers.
+#' @param alpha Can be set to a different transparency or NA if an alpha scale is needed (not recommended).
+#' @param width Can be set to a number between 0 and 1, with lower numbers increasing space between boxes.
+#' @param fontsize Can be used to adjust the size of the count. Use a number equivalent to a standard size in points.
 #'
 #' @export
 #'
-geom_boxandwhisker <- function (outlier = TRUE, count = TRUE, middlepoint = "mean", whiskerbar = TRUE) {
+geom_boxandwhisker <- function (outlier = TRUE, count = TRUE, middlepoint = "mean", whiskerbar = TRUE,
+                                alpha = .8, width = .9, fontsize = 9, ...) {
 
   # Box and Whiskers - these functions set up for the stat_summary functions
 
@@ -164,14 +168,14 @@ geom_boxandwhisker <- function (outlier = TRUE, count = TRUE, middlepoint = "mea
 
   # For the horizontal bars on the ends of the whiskers (errorbar)
   low_bar <- function(x) {
-    r <- quantile(x, probs = c(0.05, 0.25))
-    names(r) <- c("ymin", "ymax")
-    r
+    range <- quantile(x, probs = c(0.05, 0.25))
+    names(range) <- c("ymin", "ymax")
+    range
   }
   high_bar <- function(x) {
-    r <- quantile(x, probs = c(0.75, 0.95))
-    names(r) <- c("ymin", "ymax")
-    r
+    range <- quantile(x, probs = c(0.75, 0.95))
+    names(range) <- c("ymin", "ymax")
+    range
   }
 
   # For the outlier points (point)
@@ -182,7 +186,6 @@ geom_boxandwhisker <- function (outlier = TRUE, count = TRUE, middlepoint = "mea
   # For the count (text). Location changes depending on whether outlier points are included.
   if (outlier == TRUE){
     ncount <- function(x){
-      # Need to figure out how to space this away from the points
       return(c(y = min(x), label = length(x)))
     }
   } else if (outlier == FALSE){
@@ -194,25 +197,92 @@ geom_boxandwhisker <- function (outlier = TRUE, count = TRUE, middlepoint = "mea
   # Returns a list of stat_summary for the full plot
   list(
     # This makes the actual box and whisker
-    ggplot2::stat_summary(fun.data = boxplot_info, geom = "boxplot"),
+    if(!is.na(alpha))
+    ggplot2::stat_summary(fun.data = boxplot_info, geom = "boxplot", position = ggplot2::position_dodge(0.9),
+                          alpha = alpha, width = width, ...),
+    if(is.na(alpha))
+      ggplot2::stat_summary(fun.data = boxplot_info, geom = "boxplot", position = ggplot2::position_dodge(0.9),
+                            width = width, ...),
+
     # This adds the outlier points
     if (outlier)
-      ggplot2::stat_summary(fun = outlier_points, geom= "point", position = position, size = 1.5 * pointsize),
-    # This adds the count
+      ggplot2::stat_summary(fun = outlier_points, geom= "point", position = ggplot2::position_dodge(0.9)),
+
+    # This adds the count (vjust spaces it away from the plot)
     if (count)
-      ggplot2::stat_summary(fun.data = ncount, geom = "text"),
+      ggplot2::stat_summary(fun.data = ncount, geom = "text", vjust = 1.5, position = ggplot2::position_dodge(0.9),
+                            size = fontsize / ggplot2::.pt),
+
     # This makes the point at the mean or 90th percentile
-    if (diamond == "mean")
-      ggplot2::stat_summary(fun = mean, geom = "point", shape = 8, colour = "black"),
-    if (diamond == "90th")
-      ggplot2::stat_summary(fun = quantile, fun.args = list(probs = 0.9),  geom = "point", shape = 8, colour = "black"),
+    if (middlepoint == "mean")
+      ggplot2::stat_summary(fun = mean, geom = "point", shape = 8, colour = "black", position = ggplot2::position_dodge(0.9)),
+    if (middlepoint == "90th")
+      ggplot2::stat_summary(fun = quantile, fun.args = list(probs = 0.9),  geom = "point", shape = 8, colour = "black",
+                            position = ggplot2::position_dodge(0.9)),
+
     # This adds the horizontal bars on the whiskers
     if (whiskerbar)
-      ggplot2::stat_summary(fun.data = low_bar, geom = "errorbar"),
+      ggplot2::stat_summary(fun.data = low_bar, geom = "errorbar", position = ggplot2::position_dodge(0.9),
+                            width = 0.9 * width),
     if (whiskerbar)
-      ggplot2::stat_summary(fun.data = high_bar, geom = "errorbar")
+      ggplot2::stat_summary(fun.data = high_bar, geom = "errorbar", position = ggplot2::position_dodge(0.9),
+                            width = 0.9 * width)
   )
 
 }
 
+
+
+iris <- iris %>%
+  mutate(binpetal = as.factor(round(Petal.Width/.5)*.5))
+ggplot(iris, aes(x = Species, y = Sepal.Width)) +
+  geom_boxandwhisker(width = .5, fill = "purple", fontsize = 12) +
+  theme_bw(base_size = 15)
+
+
+########################################################################################################################*
+
+#' Theme function for BC defaults
+#' Automatically sets BC color palette.
+#'
+#' @param base_size Can be set to adjust default font sizes
+#' @param base_family Can be set to change default font (R isn't great at fonts, so be careful)
+#'
+#' @export
+#'
+theme_bc <- function (base_size = 11, base_family = "", ...) {
+  theme_bw(base_size = base_size, base_family = base_family, ...) %+replace%
+    theme(plot.background = element_rect(fill = "transparent", color = NA),
+          panel.border = element_rect(fill = NA, colour = "#43525a"),
+          panel.grid = element_line(colour = "#f1f2f2"),
+          panel.grid.minor = element_blank(),
+          strip.background = element_rect(fill = "#332a86", colour = "#43525a"),
+          strip.text = element_text(colour = "white", size = rel(0.8),
+                       margin = margin(0.8 * base_size/2, 0.8 * base_size/2, 0.8 * base_size/2, 0.8 * base_size/2)),
+          legend.key = element_rect(fill = "transparent", colour = NA), complete = TRUE,
+          legend.background = element_rect(fill = "transparent"),
+          legend.box.background = element_rect(fill = "transparent", color = NA))
+}
+
+ggplot(iris, aes(x = Sepal.Width, y = Petal.Width)) +
+  geom_point() +
+  facet_wrap(~Species) +
+  theme_bc()
+
+########################################################################################################################*
+#' Creates a png file of plots with automatic scaling for adding to word documents
+#'
+#' @param plot Plot to be exported
+#' @param filename Desired file name in quotes without file extension.
+#' @param vscale Default height is 4", based on typical plot ratios to copy into a word doc.  Increase scale to multiply size.
+#' @param hscale Default width is 6.5", only change the scale if plot is being added to a different page width.
+#'
+#' @export
+#'
+export_plot <- function(plot, filename = "DRAFT R Plot", vscale = 1){
+
+  ggplot2::ggsave(plot = plot, filename = paste(filename, ".png", sep = ""),
+                  width = 6.5*hscale, height = 4*vscale, units = "in", dpi = 300, bg = "transparent")
+
+}
 
